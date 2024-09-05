7a1ce1b1ea09f376103535b3f17748581acfc1a6
daftcucumber <117169129+daftcucumber@users.noreply.github.com>
2024-09-05 12:03:30 +0800

    Universality app.py
    change incom of func index

--- app.py
+++ app.py
@@ -10,19 +10,36 @@ app= Flask(__name__)
 app.config['MYSQL_HOST']="localhost"
 app.config['MYSQL_USER']="root"
 app.config['MYSQL_PASSWORD']=""
-app.config['MYSQL_DB']='python_web_mysql'
+app.config['MYSQL_DB']=''
 
 mysql=MySQL(app)
 
+table_name = ""
 
 @app.route('/')
-def index():
-    cur=mysql.connection.cursor()
-    data=cur.execute("select * from employee_detail")
-    if data>0:
-        employees=cur.fetchall()
-    
-    return render_template('index.html',employees=employees)
+
+from flask import render_template  
+  
+def index(table_name, conditions=None):  
+    cur = mysql.connection.cursor()  
+    query = f"SELECT * FROM {table_name}"  
+    if conditions:  
+        query += f" WHERE {conditions}"  
+      
+    try:  
+        cur.execute(query)  
+        datas = cur.fetchall()  
+    except Exception as e:  
+        # perror  
+        print(f"Error executing query: {e}")  
+        datas = []   
+      
+    # ensure data is defined  
+    if not isinstance(datas, list):  
+        datas = []  
+      
+    return render_template('index.html', employees=data)
+
 
 
 if __name__=="__main__":
